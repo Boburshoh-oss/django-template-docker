c5728a52aed506a5d312334b7ae07a50b7c18bd2
Botirov Boburbek <74315658+Boburshoh-oss@users.noreply.github.com>
2024-06-26 14:16:00 +0500

    Update production.py
    Signed-off-by: Botirov Boburbek <74315658+Boburshoh-oss@users.noreply.github.com>

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -9,13 +9,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, "../", "staticfiles")  # type: ignore
 # settings.py
 import sentry_sdk
 
-# sentry_sdk.init(
-#     dsn="https://d4f5fea8d87dfdf5f68f6abb898e2651@o4506259320274944.ingest.us.sentry.io/4507021769048064",
-#     # Set traces_sample_rate to 1.0 to capture 100%
-#     # of transactions for performance monitoring.
-#     traces_sample_rate=1.0,
-#     # Set profiles_sample_rate to 1.0 to profile 100%
-#     # of sampled transactions.
-#     # We recommend adjusting this value in production.
-#     profiles_sample_rate=1.0,
-# )
